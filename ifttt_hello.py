2770a7e53abae8ec62ed1da9374d978c0feaf159
JamesTsaiJT <43771177+JamesTsaiJT@users.noreply.github.com>
2022-06-10 18:24:36 +0800

    Update ifttt_hello.py

--- ifttt_hello.py
+++ ifttt_hello.py
@@ -1,5 +1,5 @@
 import requests
-webhook_key = "b7_ILrDHhMK9hW77ZRgGkq"#填入Webhook金鑰
+webhook_key = "b7_ILrDHhMK9hW77ZRgGk"#填入Webhook金鑰
 trigger_name = "Lesson9"#填入觸發條件名稱
 url ='https://maker.ifttt.com/trigger/'+trigger_name+'/with/key/'+webhook_key+'?value1=hello world'
 requests.get(url)
